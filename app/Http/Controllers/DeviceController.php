fa0303f43577ab6052f56785299a2574fa4aa78a
Niklas Stambor <7977327+nklasio@users.noreply.github.com>
2022-03-20 20:45:01 +0100

    DeviceController: Optimize relationship loading

--- app/Http/Controllers/DeviceController.php
+++ app/Http/Controllers/DeviceController.php
@@ -11,10 +11,13 @@ use Illuminate\Support\Facades\Auth;
 
 class DeviceController extends Controller {
     public function index() {
-        return view('devices')->withDevices(Auth::user()->devices);
+        $user = Auth::user()->load('devices');
+
+        return view('devices', ['devices' => $user->devices]);
     }
 
     public function show(Device $device) {
+        $device->load('backups');
         return view('device.device')->withDevice($device);
     }
 }
